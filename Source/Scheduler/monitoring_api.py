37e68ce0af071ffffc4f475a208f5ba5b9654a9c
Janmejay50010 <janmejay50010@gmail.com>
2021-08-27 10:24:24 +0530

    Added monitoring_api for scheduler

---
+++ Source/Scheduler/monitoring_api.py
@@ -0,0 +1,27 @@
+from kafka import KafkaProducer
+import json
+import datetime
+from time import sleep
+import threading
+
+kafkaIPPort = '52.15.89.83:9092'
+
+producer = KafkaProducer(bootstrap_servers=kafkaIPPort,
+value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
+
+
+def heart_beat(module_name):
+    while True:
+        currtime = str(datetime.datetime.utcnow())
+        message = {
+            'moduleName':module_name,
+            'currentTime':currtime
+        }
+        producer.send('monitor',message)
+        sleep(5)
+
+def monitor_thread(module_name):
+    t = threading.Thread(target=heart_beat,args=(module_name,))
+    t.daemon = True
+    t.start()

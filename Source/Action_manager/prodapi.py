0021854e1a97c999b8d580c20577f7d905738c83
Janmejay50010 <janmejay50010@gmail.com>
2021-09-22 07:04:41 +0530

    Added producer api for Action Manaer

---
+++ Source/Action_manager/prodapi.py
@@ -0,0 +1,10 @@
+from kafka import KafkaProducer
+import json
+
+kafkaIPPort = '52.15.89.83:9092'
+
+producer = KafkaProducer(bootstrap_servers = kafkaIPPort,
+value_serializer=lambda v: json.dumps(v).encode('utf-8'))        
+def send_message(topic,message):
+    producer.send(topic, value = message)
+    producer.flush()
